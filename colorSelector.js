38125956104f4019512bacd28037049e34d9add3
~ Yasminee ~ <user@Yasmin-s.local>
2024-09-19 09:28:42 +0700

    make repo and push

--- colorSelector.js
+++ colorSelector.js
@@ -1,17 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Logika untuk pemilih warna iPhone
     const colorButtons = document.querySelectorAll('.color-btn');
     const iphoneImage = document.getElementById('iphoneImage');
 
     colorButtons.forEach(button => {
         button.addEventListener('click', function() {
             const newImage = this.getAttribute('data-image');
-            // Tambahkan transisi halus untuk perubahan gambar
-            iphoneImage.classList.remove('active'); // Hapus kelas aktif untuk memicu efek fade-out
+            iphoneImage.classList.remove('active');
             setTimeout(() => {
-                iphoneImage.src = newImage; // Ubah gambar setelah fade-out
-                iphoneImage.classList.add('active'); // Tambah kelas aktif untuk memicu efek fade-in
-            }, 300); // Tunggu 300ms agar fade-out selesai
+                iphoneImage.src = newImage; 
+                iphoneImage.classList.add('active'); 
+            }, 300); 
         });
     });
 });
